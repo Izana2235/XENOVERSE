b9db422144877ca9c9a8f08d1e45726a33fecbbd
Ishikawa2205 <johnjeffreycacho@gmail.com>
2026-02-17 23:41:31 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.getElementById("sidebar");
-
-function openSidebar() {
-    sidebar.classList.add("active");
-}
-
-function closeSidebar() {
-    sidebar.classList.remove("active");
-}
