5e3b3bae96a6a46754b0bb442bf93725c1c9b06b
Ishikawa2205 <johnjeffreycacho@gmail.com>
2026-02-17 23:48:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const sidebar = document.getElementById("sidebar");
-
 function openSidebar() {
-    sidebar.classList.add("active");
+    document.getElementById("sidebar").classList.add("active");
 }
 
 function closeSidebar() {
-    sidebar.classList.remove("active");
+    document.getElementById("sidebar").classList.remove("active");
 }
